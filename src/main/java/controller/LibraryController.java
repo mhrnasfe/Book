1a00e2310a8d45bf6c1bc8c194a633b4ef6ef449
abolfazl <a.b.f.m.98@gmail.com>
2024-02-15 15:53:01 +0330

    این لایه  controller این لایه کارش ارتباط بین لایه view , model هست ک مدل میشه س دسته ک ت پکیچ بندی ها مشخص کردمش

--- src/main/java/controller/LibraryController.java
+++ src/main/java/controller/LibraryController.java
@@ -1,4 +1,18 @@
 package controller;
 
+import model.entity.Book;
+import model.service.LibraryService;
+
+import java.util.List;
+
 public class LibraryController {
+    LibraryService libraryService = new LibraryService();
+
+    public void saveBook(Book book) {
+        libraryService.saveBook(book);
+    }
+
+    public List<Book> findAll() {
+        return libraryService.findAll();
+    }
 }
