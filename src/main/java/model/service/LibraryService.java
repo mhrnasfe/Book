19110bf4df39acbd4c187162f56f435bc776a70c
abolfazl <a.b.f.m.98@gmail.com>
2024-02-15 15:51:29 +0330

    این لایه  service اینجا کد های بیزنسی قرار میگیره (بعدا بیشتر راجبش حرف میزنیم)

--- src/main/java/model/service/LibraryService.java
+++ src/main/java/model/service/LibraryService.java
@@ -1,4 +1,18 @@
 package model.service;
 
+import model.entity.Book;
+import model.repository.LibraryRepository;
+
+import java.util.List;
+
 public class LibraryService {
+    LibraryRepository libraryRepository = new LibraryRepository();
+
+    public void saveBook(Book book) {
+        libraryRepository.saveBook(book);
+    }
+
+    public List<Book> findAll() {
+        return libraryRepository.findAll();
+    }
 }
