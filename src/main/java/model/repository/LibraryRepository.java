4e29300f83e52a0647e77cbffac63e1089c2d06d
abolfazl <a.b.f.m.98@gmail.com>
2024-02-15 15:52:11 +0330

    این لایه  repository دیتا میاد اینجا و عمل crud  انجام میشه

--- src/main/java/model/repository/LibraryRepository.java
+++ src/main/java/model/repository/LibraryRepository.java
@@ -1,4 +1,18 @@
 package model.repository;
 
+import model.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryRepository {
+    List<Book> library = new ArrayList<>();
+
+    public void saveBook (Book book) {
+        library.add(book);
+    }
+
+    public List<Book> findAll() {
+        return library;
+    }
 }
